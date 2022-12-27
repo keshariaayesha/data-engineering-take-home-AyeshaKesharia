a23b909e15a9abcce2a7eb471730911a71e785ad
Ayesha Kesharia <keshariaayesha@gmail.com>
2022-12-27 17:25:27 +0530

    Updated transformation.py file

--- scripts/transformation.py
+++ scripts/transformation.py
@@ -30,7 +30,7 @@ print(df)
 
 #establishing the connection
 conn = psycopg2.connect(
-    database="postgres", user='postgres', password='postgrespw', host='host.docker.internal', port= '49155'
+    database="postgres", user='postgres', password='postgrespw', host='host.docker.internal', port= '49156'
 )
 
 #Creating a cursor object using the cursor() method
@@ -38,3 +38,16 @@ cursor = conn.cursor()
 
 cursor.execute('CREATE TABLE IF NOT EXISTS user_logins(user_id  varchar(128), device_type varchar(32),masked_ip varchar(256),masked_device_id varchar(256),locale varchar(32),app_version integer,create_date date)')
 print('Creation of table succeeded')
+
+
+for i in df.index:
+    cols  = ','.join(list(df.columns))
+    vals  = [df.at[i,col] for col in list(df.columns)]
+    query = "INSERT INTO user_logins VALUES('%s','%s','%s','%s','%s','%s','%s')" % (vals[0], vals[1], vals[2],vals[3],vals[4],vals[5],vals[6])
+    cursor.execute(query)
+
+# fetching all rows
+sql1='''select * from user_logins'''
+cursor.execute(sql1)
+for i in cursor.fetchall():
+    print(i)
